a3c036ed4b320d4d12c61756cdaae1ff14e36a7b
j113203 <j113203@users.noreply.github.com>
2018-01-11 23:42:45 +0800

    1.1
    symptoms

--- js/index.js
+++ js/index.js
@@ -1,22 +1,69 @@
-var background = document.getElementById("background");
-var hideCenter = document.getElementById("hideCenter");
-var loading = document.getElementById("loading");
-var splash = document.getElementById("splash");
+/**
+ *  Perferences
+ */
+var perferences_language = "zh-HK";
 
-background.addEventListener('loadedmetadata', function () {
-    this.currentTime = 0;
-    this.play();
-}, false);
+/**
+ *  Splash Screen
+ */
 
-background.addEventListener("timeupdate", function () {
+(function (screen_splash) {
+    var background = document.getElementById("background");
+    var hideCenter = document.getElementById("hideCenter");
+    var loading = document.getElementById("loading");
+    var splash = document.getElementById("splash");
+    var loading_value = document.getElementById("loading\.value");
 
-    if (this.currentTime >= 4.6) {
-        this.pause();
-        splash.classList.add("animated", "fadeOut");
-    } else if (this.currentTime >= 1.0) {
-        hideCenter.style.display = "block";
-        loading.style.display = "block";
-        loading.classList.add("animated", "fadeIn");
-    }
+    background.src = "video/intro.mp4";
+    background.muted = true;
+    background.addEventListener('loadedmetadata', function () {
+        this.currentTime = 0;
+        this.play();
+    }, false);
+
+    background.addEventListener("timeupdate", function () {
+
+        loading_value.style.width = ((this.currentTime / 5) * 250) + "px";
+
+        if (this.currentTime >= 4.6) {
+            this.pause();
+            splash.classList.add("animated", "fadeOut");
+            setTimeout(function () {
+                if (splash != null) {
+                    document.body.removeChild(splash);
+                    splash = null;
+                    screen_Symptoms();
+                }
+            }, 1000);
+
+        } else if (this.currentTime >= 1.0) {
+            hideCenter.style.display = "block";
+            loading.style.display = "block";
+            loading.classList.add("animated", "fadeIn");
+        }
+
+    });
+})();
+
+/**
+ *  Load Symptoms
+ */
+var screen_Symptoms = (function () {
+    var screen_symptomsInput = document.getElementById("screen\.symptomsInput");
+    var logo = document.getElementById("health\.maid");
+    screen_symptomsInput.style.display = "block";
+    screen_symptomsInput.classList.add("animated", "fadeIn");
+    logo.style.display = "block";
+    logo.classList.add("animated", "fadeIn");
+
+    var screen_symptomsInput_human = document.getElementById("screen\.symptomsInput\.human");
+    var screen_symptomsInput_human_onclick = function () {
+        console.log("cliked");
+    };
+    screen_symptomsInput_human.addEventListener("click", screen_symptomsInput_human_onclick, false);
+
+    var screen_symptomsInput_list = document.getElementById("screen\.symptomsInput\.list");
+    
 
 });
+
